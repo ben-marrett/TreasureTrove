5b42938be3094a8a6be4f13222e3a994e49ad1aa
ben-marrett <benteiko@gmail.com>
2023-10-06 23:15:36 +1300

    add homepage image and CSS

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from 'react'
+import treasureImage from '../images/treasure.jpg'
 
 function Welcome() {
   return (
     <div className="Welcome">
       <h1>Welcome to Treasure Trove</h1>
+      <img
+        src={treasureImage}
+        alt="Treasure Book"
+        className="fade-image treasure-image"
+      />
       <p>An app for storing valuable finds online.</p>
       <button className="button">Get Started</button>
     </div>
